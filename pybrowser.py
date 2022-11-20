04cee948f18109fbfc4d9c02b0e89b576b29a4bd
mcjka <95513381+mcjka@users.noreply.github.com>
2022-11-19 18:19:11 -0800

    Update pybrowser.py

--- pybrowser.py
+++ pybrowser.py
@@ -1,12 +1,32 @@
-from tkinterweb import HtmlFrame #import the HTML browser
-try:
-  import tkinter as tk #python3
-except ImportError:
-  import Tkinter as tk #python2
+# Import the required libraries
+from tkinter import *
+import webview
+from os.path import exists
+###########
+import tkinter as tk
+from tkinter import simpledialog
+ROOT = tk.Tk()
+USER_INP =''
+# the input dialog
 
-root = tk.Tk() #create the tkinter window
-frame = HtmlFrame(root) #create HTML browser
+USER_INP = simpledialog.askstring(title="URLBAR",
+                                prompt="Enter Website URL:")
 
-frame.load_website("http://google.com") #load a website
-frame.pack(fill="both", expand=True) #attach the HtmlFrame widget to the parent window
-root.mainloop()
+# check it out
+
+##########
+
+
+
+# Create an instance of tkinter frame or window
+
+bookmarks = 'bookmarks'
+# Set the size of the window
+
+# Create a GUI window to view the HTML content
+#Ask user for website
+
+webview.create_window(USER_INP, USER_INP)
+webview.start()
+
+
